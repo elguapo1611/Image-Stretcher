ec72f8fee4d46e9023aaed018aedd873ded5cabf
Jon Phillips <jphillips@opthumb.com>
2011-03-21 20:02:34 -0600

    added js conversions

--- app/controllers/image_conversions_controller.rb
+++ app/controllers/image_conversions_controller.rb
@@ -2,26 +2,17 @@ class ImageConversionsController < ApplicationController
   include ImageConversionsHelper
   
   def index
-    
-    @converted_files = []
-    
-    begin 
-      # files.each_with_index do |file, i|
-      #         
-      #       end
-    rescue => e
-      # puts files.inspect
-    end
-
-    # puts @converted_files.inspect
-    
-    
+    @path = "#{RAILS_ROOT}/public/images/library/"
+    @src_url = "/images/library/"
+    @images = Dir.glob("#{@path}*.jpg").map{|image| File.basename(image)}
   end
   
   def convert_image
-    file = CGI::unescape(params[:path])
-    converted_url = convert_image_from_path(file, "#{RAILS_ROOT}/public/test/")
-    @converted_file = {:src => file_name_without_directory(file), :converted => converted_url}
+    path = CGI::unescape(params[:path])
+    file = params[:file_name]
+    puts "#{path}#{file}"
+    converted_url = convert_image_from_path("#{path}#{file}", "#{RAILS_ROOT}/public/images/test/")
+    @converted_file = {:src => File.basename(file), :converted => converted_url}
     render :text => @converted_file.to_json
   end
 end
--- app/helpers/image_conversions_helper.rb
+++ app/helpers/image_conversions_helper.rb
@@ -2,47 +2,51 @@ module ImageConversionsHelper
   require 'RMagick'
   include Magick
   
-  def convert_image_from_path(src, output_dir)
+  def convert_image_from_path(src, output_dir, options = {})
     begin
       img =  Image.read(src).first
     rescue => e
       return {:errors => e}
     end
     
-    image_name = file_name_without_directory(src)
-    
-    puts image_name
-    
+    image_name = file_name_without_directory_or_extension(src)
     converted_img = stretch_convert_image(img)
-    # write_image(converted_img, output_path, name)
+    converted_name = converted_file_name(image_name)
+    write_image(converted_img, output_dir, converted_name)
+    converted_name
   end
   
   def convert_image_from_url(src_url, output_dir)
     img =  Image.read(file).first
     converted_img = convert_image(img)
-    write_image(converted_img, output_path)
+    write_image(converted_img, output_dir)
   end
   
 private 
 
   # takes an rmagick image and returns a new converted image
-  def stretch_convert_image(img, width = 640, height = 480)
-    new_image = img.crop(0,0,width,4)
-    scaled_image = new_image.adaptive_resize(width, height)
-    # converted_path = file_name_without_directory_or_extension(file)
+  def stretch_convert_image(img, start_x = 0, start_y = 0, width = 640, height = 480)
+    new_image = img.crop(0,0,width,4).adaptive_resize(width, height)
+    return new_image
   end
   
   def write_image(img, path, name)
+    puts img.inspect
     img.write("#{path}#{name}")
   end
   
   # returns the filename
   def file_name_without_directory(filename)
-    filename
+    File.basename(filename)
   end
   
   def file_name_without_directory_or_extension(filename)
-    file_name_without_directory(filename)
+    File.basename(filename, ".jpg")
   end
+  
+  def converted_file_name(original_file_name)
+    "#{original_file_name}_converted.jpg"
+  end
+  
   
 end
